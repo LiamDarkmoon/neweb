93e67a05df89966a8ac3e6c5edcd9424e20253ce
Liam Darkmoon <liamdarkmoon@gmail.com>
2022-06-13 19:16:32 -0300

    VERSION 1.999

--- mt/js/script.js
+++ mt/js/script.js
@@ -1,19 +1,35 @@
-const cartButtons = document.querySelectorAll('.cart-button');
+let root = document.querySelector(':root');
+let color = '#5f4b8b';
+console.log(color);
 
-cartButtons.forEach(button => {
-    button.addEventListener('click', cartClick);
+root.addEventListener("change", () => {
+  if (color === '#5f4b8b') {
+    root.style.setProperty('--main-color', '#f5df4d');
+    root.style.setProperty('--secondary-color', '#5f4b8b');
+    color = '#f5df4d';
+  } else if (color === '#f5df4d') {
+    root.style.setProperty('--main-color', '#5f4b8b');
+    root.style.setProperty('--secondary-color', '#f5df4d');
+    color = '#5f4b8b';
+  }
 });
 
-function cartClick () {
-    let button = this;
-    button.classList.add('clicked');
+const cartButtons = document.querySelectorAll(".cart-button");
+
+cartButtons.forEach((button) => {
+  button.addEventListener("click", cartClick);
+});
+
+function cartClick() {
+  let button = this;
+  button.classList.add("clicked");
 }
 
-cartButtons.forEach(button => {
-    button.addEventListener('animationend', removeClick);
+cartButtons.forEach((button) => {
+  button.addEventListener("animationend", removeClick);
 });
 
-function removeClick () {
-    let button = this;
-    button.classList.remove('clicked');
+function removeClick() {
+  let button = this;
+  button.classList.remove("clicked");
 }
